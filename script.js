631ea3a3857850d3640d1d80e73a85cb4995f3b6
Have Samuel <samiehave9@gmail.com>
2024-02-29 17:49:50 +0300

    Adding the Button

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// calculating the payment and tip amount
-function calculateTip() {
-  const bill = document.getElementById('bill').value;
-  // Tip buttons
-  const tip = document.querySelectorAll('tip-button');
-  // Custom tip
-  const customTip = document.getElementById('custom').value;
-  // Number of people
-  const noPple = document.getElementById('people').value;
-}
-// Button click
+// Variables
+// Bill amount
+const bill = document.getElementById('bill').value;
+// Tip buttons
+const tip = document.querySelectorAll('tip-button');
+// Custom tip
+const customTip = document.getElementById('custom').value;
+// Number of people
+const noPple = document.getElementById('people').value;
+// Reset button
+const reset = document.getElementById('reset');
+
