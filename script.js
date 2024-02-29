f3d0e4a913e4db9c409683539580f80912e46f18
Have Samuel <samiehave9@gmail.com>
2024-02-29 17:45:46 +0300

    Creating variables

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // calculating the payment and tip amount
 function calculateTip() {
-    
+  const bill = document.getElementById('bill').value;
+  // Tip buttons
+  const tip = document.querySelectorAll('tip-button');
+  // Custom tip
+  const customTip = document.getElementById('custom').value;
+  // Number of people
+  const noPple = document.getElementById('people').value;
 }
+// Button click
