1948a18a713fd30c53a8783bd1f0c0f3952ca653
Have Samuel <samiehave9@gmail.com>
2024-03-01 16:41:42 +0300

    Removing the the unwanted code

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // Variables
-// Bill amount
 const bill = document.getElementById('bill').value;
-// Tip buttons
 const tip = document.querySelectorAll('tip-button');
-// Custom tip
 const customTip = document.getElementById('custom').value;
-// Number of people
 const noPple = document.getElementById('people').value;
-// Reset button
 const resetBtn = document.getElementById('reset');
 
 // Functions
