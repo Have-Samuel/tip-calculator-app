040010f8162fd57b572022ba24123bd433a7726c
Have Samuel <samiehave9@gmail.com>
2024-03-06 10:26:55 +0300

    Making the Reset Button reset all the Input fields

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
 // Variables
 const billAmount = document.getElementById('bill');
-const buttons = document.querySelectorAll('.tip-button button');
-const billTipAmount = document.getElementById('tip-amount');
 const customTipPercentage = document.getElementById('custom');
 const numPeople = document.getElementById('people');
+const tipAmount = document.getElementById('tip-amount');
 const billTotalPerPerson = document.getElementById('total');
 const resetBtn = document.getElementById('reset');
+// const buttons = document.querySelectorAll('.tip-button button');
 
-// Calculating the Tip
-function calculateTip() {
-  tip.forEach((button) => {
-    button.addEventListener('click', () => {
-      const tipAmount = button.value;
-      const tipValue = (bill * tipAmount) / noPple;
-      const total = bill / noPple + tipValue;
-      document.getElementById('tip-amount').textContent = tipValue;
-      document.getElementById('total').textContent = total;
-    });
-  });
-}
-
-// Custom tip
-function custom() {
-  customTip.addEventListener('input', () => {
-    const tipValue = (bill * customTip) / noPple;
-    const total = bill / noPple + tipValue;
-    document.getElementById('tip-amount').textContent = tipValue;
-    document.getElementById('total').textContent = total;
-  });
-}
-
-// Reset
-function reset() {
-
-}
-
-// Event listeners
-calculateTip();
-custom();
-reset();
+resetBtn.addEventListener('click', (eve) => {
+  billAmount.value = '0';
+  customTipPercentage.value = '';
+  numPeople.value = '';
+  tipAmount.textContent = '$0.00';
+  billTotalPerPerson.textContent = '$0.00';
+});
