f590d5c627c1bd9ac162a58e224bd8274189cc46
Have Samuel <samiehave9@gmail.com>
2024-03-14 15:28:31 +0300

    Showing digits in the input fields

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const billAmount = document.getElementById('bill');
-const numPeople = document.getElementById('people');
+const billInput = document.getElementById('bill');
+const peopleInput = document.getElementById('people');
 const tipPerPerson = document.getElementById('tip-amount');
 const totalPerPerson = document.getElementById('total-amount');
 const buttons = document.querySelectorAll('.tip-button');
@@ -7,10 +7,10 @@ const buttons = document.querySelectorAll('.tip-button');
 const customTip = document.getElementById('custom');
 const resetBtn = document.getElementById('reset');
 
-billAmount.value = '0.0';
-numPeople.value = '1';
-tipPerPerson.textContent = `$${(0.00).toFixed(2)}`;
-totalPerPerson.textContent = `$${(0.00).toFixed(2)}`;
+billInput.value = '0.0';
+peopleInput.value = '1';
+tipPerPerson.innerHTML = `$${(0.00).toFixed(2)}`;
+totalPerPerson.innerHTML = `$${(0.00).toFixed(2)}`;
 
 let billValue = 0.0;
 let peopleValue = 1;
@@ -19,21 +19,21 @@ let tipValue = 0.15;
 // Calculating the Tip and Total Amount
 function calculateTip() {
   if (peopleValue >= 1) {
-    const tipPerPerson = (billValue * tipValue) / peopleValue;
-    const totalPerPerson = (billValue * tipPerPerson) / peopleValue;
-    tipPerPerson.textContent = `${tipPerPerson.toFixed(2)}`;
-    totalPerPerson.textContent = `${totalPerPerson.toFixed(2)}`;
+    const tipAmount = (billValue * tipValue) / peopleValue;
+    const total = (billValue * tipAmount) / peopleValue;
+    tipPerPerson.innerHTML = `${tipAmount.toFixed(2)}`;
+    totalPerPerson.innerHTML = `${total.toFixed(2)}`;
   }
 }
 
 // Display the tip amount per person
 function billInputFun() {
-  billValue = parseFloat(billAmount.value);
+  billValue = parseFloat(billInput.value);
   calculateTip();
 }
 // Display the total amount per person
 function numPeopleFun() {
-  peopleValue = parseFloat(numPeople.value);
+  peopleValue = parseFloat(peopleInput.value);
   calculateTip();
 }
 
@@ -41,16 +41,16 @@ function numPeopleFun() {
 function clickHandler(e) {
   buttons.forEach((button) => {
     button.classList.remove('active');
-    if (e.target.textContent === button.textContent) {
+    if (e.target.innerHTML === button.innerHTML) {
       button.classList.add('active');
-      tipValue = parseFloat(button.textContent) / 100;
+      tipValue = parseFloat(button.innerHTML) / 100;
     }
   });
   calculateTip();
 }
 
-billAmount.addEventListener('input', billInputFun);
-numPeople.addEventListener('input', numPeopleFun);
+billInput.addEventListener('input', billInputFun);
+peopleInput.addEventListener('input', numPeopleFun);
 buttons.forEach((button) => {
   button.addEventListener('click', clickHandler);
 });
